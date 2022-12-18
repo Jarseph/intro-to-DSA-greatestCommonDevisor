3dea0590fcbbe8992bbc3459a09ceff5ab0ec708
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-12-17 18:52:25 -0700

    Create gcd.js

---
+++ gcd.js
@@ -0,0 +1,50 @@
+/**
+ * Write a function named gcd1 that implements algorithm 1 here
+ */
+function gcd1(a, b) {
+  if(a == 0) {
+    return b;
+  }
+  if(b === 0) {
+    return a;
+  }
+  while(b !== 0) {
+    let r = a % b;
+    a = b;
+    b = r;
+  }
+  return a;
+}
+
+/**
+ * Write a function named gcd2 that implements algorithm 2 here
+ */
+function gcd2(a, b) {
+  if (a < 0 || b < 0) return;
+  if(a === 0) return b;
+  if(b === 0) return a;
+  
+  let divA = [1, a];
+  let divB = [1, b];
+  let common = [];
+  let num = 0;
+
+  for(let i = 2; i < a - 1; i++) {
+    if(a % i === 0) divA.push(i);
+  }
+  for(let i = 2; i < b - 1; i++) {
+    if(b % i === 0) divB.push(i);
+  }
+  divA.forEach((n) => {
+    if(divB.includes(n)) {
+      common.push(n);
+    }
+  });
+  let largest = Math.min(...common);
+  common.forEach((n) => {
+    if(n > largest) largest = n;
+  });
+  return largest;
+}
+
+module.exports = { gcd1, gcd2 };
